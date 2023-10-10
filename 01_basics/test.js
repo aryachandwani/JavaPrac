2d9b264e39b79bce9115eb789bff668cd80d7a41
Arya Chandwani <97949655+aryachandwani@users.noreply.github.com>
2023-10-10 13:37:18 +0000

    New Commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,11 @@
 console.log("hello")
+
+const accountId = 123423
+var accountName = "Arya PC"
+let accountEmail
+
+
+// Prefer not to use var at all!
+// If something is not declared, it is undefined!
+
+console.table([accountId, accountEmail, accountName]);
---
+++ 02_datatypes/test.js
@@ -0,0 +1,5 @@
+let state;
+
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
