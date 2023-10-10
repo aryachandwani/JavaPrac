7fa1548bab0aac9640ad9905aa56ea0d486202ed
Arya Chandwani <97949655+aryachandwani@users.noreply.github.com>
2023-10-10 13:52:29 +0000

    Conv of Datatyped

---
+++ 01_basics/03_convOps.js
@@ -0,0 +1,30 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//  NaN --> Not a number.
+
+// null converted to 0
+// undefinded converted to NaN 
+// string converted to Nan
+// bool to 0/1
+
+let isLoggedIn = undefined
+
+let boolLoggedIn = Boolean(isLoggedIn)
+// console.log(boolLoggedIn);
+
+// Null, Undefined --> false
+// Empty string ka false, else true
+
+
+let someNumber = undefined
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber);
